6059c2fa10086bd80b56e1274e5bea9355e80c90
Ryan J. Geyer <me@ryangeyer.com>
2013-01-30 14:43:35 -0800

    All tied up for now

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,24 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Ryan J. Geyer"
+maintainer_email "me@ryangeyer.com"
 license          "All rights reserved"
 description      "Installs/Configures gitolite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+depends "rightscale"
+depends "git"
+depends "perl"
+
+recipe "gitolite::default", "Installs the gitolite binaries and sets up a single instance"
+
+attribute "gitolite/home",
+  :display_name => "Gitolite Instance Home Directory",
+  :description => "The full path to the home directory for Gitolite",
+  :default => "/mnt/storage/gitolite",
+  :recipes => ["gitolite::default"]
+
+attribute "gitolite/ssh_key",
+  :display_name => "Gitolite Private Key",
+  :description => "Private RSA (or DSA) key material to be used when initializing the gitolite repository/home. Set to ignore for a new key to be automatically generated.",
+  :required => false,
+  :recipes => ["gitolite::default"]
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,118 @@
 # Cookbook Name:: gitolite
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Ryan J. Geyer
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rightscale_marker :begin
+
+gitolite_install = ::File.join(node["gitolite"]["install_dir"], "install")
+ssh_dir = ::File.join(node["gitolite"]["home"], ".ssh")
+private_key = ::File.join(ssh_dir, "id_rsa")
+public_key = ::File.join(ssh_dir, "id_rsa.pub")
+
+home_dir_parent = node["gitolite"]["home"].gsub(::File.basename(node["gitolite"]["home"], ""), "")
+
+# Hardcode the git base_dir which git::server uses
+node["git"]["server"]["base_path"] = ::File.join(node["gitolite"]["home"], "repositories")
+node["git"]["server"]["export_all"] = "false"
+
+case node["platform_family"]
+  when "debian"
+    su_param = "--command"
+  when "rhel"
+    su_param = "--session-command"
+end
+
+include_recipe "git"
+# git::server is included as well, but at the end of this recipe so that
+# all of the directories and configs and everything is in place before it runs
+include_recipe "perl"
+
+package "perl-Time-HiRes" if node["platform_family"] == "rhel"
+
+git node["gitolite"]["install_dir"] do
+  repository "https://github.com/sitaramc/gitolite.git"
+  reference "master"
+  action :sync
+end
+
+execute "Install gitolite binary" do
+  command "#{gitolite_install} -ln #{node["gitolite"]["bin_dir"]}"
+end
+
+directory home_dir_parent do
+  recursive true
+end
+
+group node["gitolite"]["gid"] do
+  action :create
+end
+
+user node["gitolite"]["uid"] do
+  comment "Gitolite repository"
+  gid node["gitolite"]["gid"]
+  home node["gitolite"]["home"]
+  shell "/bin/sh"
+end
+
+directory ssh_dir do
+  group node["gitolite"]["gid"]
+  owner node["gitolite"]["uid"]
+  mode 00700
+  recursive true
+  action :create
+end
+
+if node["gitolite"]["ssh_key"]
+  file private_key do
+    content node["gitolite"]["ssh_key"]
+    mode 00600
+    group node["gitolite"]["gid"]
+    owner node["gitolite"]["uid"]
+    backup false
+    action :create
+  end
+
+  bash "Create public key from private key" do
+    code <<-EOF
+ssh-keygen -N \"\" -f #{private_key} -y > #{public_key}
+    EOF
+    not_if ::File.exist?(public_key)
+  end
+else
+  execute "Create a net-new private and public SSH key" do
+    command "ssh-keygen -q -t rsa -N \"\" -f #{private_key}"
+    creates private_key
+  end
+end
+
+execute "Make sure #{node["gitolite"]["uid"]}:#{node["gitolite"]["gid"]} owns #{node["gitolite"]["home"]}" do
+  command "chown -R #{node["gitolite"]["uid"]}:#{node["gitolite"]["gid"]} #{node["gitolite"]["home"]}"
+end
+
+execute "Initialize a fresh gitolite instance (if one does not already exist)" do
+  command <<-EOF
+  su #{su_param}="gitolite setup -pk #{public_key}" #{node["gitolite"]["uid"]}
+EOF
+  creates ::File.join(node["gitolite"]["home"], "repositories", "gitolite-admin.git")
+end
+
+execute "Make sure everyone can read #{node["gitolite"]["home"]} so that git-daemon can read it" do
+  command "chmod 0775 -R #{node["gitolite"]["home"]}"
+end
+
+include_recipe "git::server"
+
+rightscale_marker :end
